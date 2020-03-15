e6a34499a8016eec411a6dce792160649b1a3c67
lawsont <lawson.thalmann@kellogg.northwestern.edu>
2020-03-15 23:07:56 +0000

    login

--- app.rb
+++ app.rb
@@ -66,3 +66,25 @@ get "/users/create" do
     users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
     view "create_user"
 end   
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
