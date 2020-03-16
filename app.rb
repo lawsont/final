db4720bd485f9a64d8c2ba49347dc28b56c74e96
lawsont <lawson.thalmann@kellogg.northwestern.edu>
2020-03-16 04:06:20 +0000

    twilio auth

--- app.rb
+++ app.rb
@@ -14,6 +14,22 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
+# put your API credentials here (found on your Twilio dashboard)
+# put your API credentials here (found on your Twilio dashboard)
+account_sid = ENV[TWILIO_ACCOUNT_SID]
+auth_token = ENV[TWILIO_AUTH_TOKEN]
+
+# set up a client to talk to the Twilio REST API
+client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+client.messages.create(
+ from: "+19723629530", 
+ to: "+18479897160",
+ body: "Hey KIEI 451!"
+)
+
+
 plant_data_table = DB.from(:plant_data)
 unique_plants_table = DB.from(:unique_plants)
 users_table = DB.from(:users)
